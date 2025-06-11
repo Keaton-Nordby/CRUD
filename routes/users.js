fe694ccade32d0b4556a7e62bae7e8d8d39238ed
Keaton Nordby <keatonnordby4@gmail.com>
2025-06-10 21:36:09 -0500

    minor progress

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,22 @@ import express from 'express';
 
 const router = express.Router();
 
+
+const users = [
+   { 
+    firstName: "John",
+    lastName : "Doe",
+    age : 25
+   }
+]
+
+// I don't need '/users' becuase 
+// all routes will start with 'users'
+// so I can abbreviate it with just '/'
 router.get('/', (req, res) => {
-    res.send('Hello');
+    console.log(users);
+
+    res.send(users);
 })
 
 export default router
